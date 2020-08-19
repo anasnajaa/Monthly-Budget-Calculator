3efe3b7808d356c8a3591df4e985a6e40c0f96b4
A.N <spidernet12@gmail.com>
2020-08-19 12:40:42 +0300

    Update index.js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,102 +1,287 @@
-const currentDatabaseId = document.cookie.split("=")[1] || util.uuid();
-let budgetsList = [];
-document.cookie = "id="+currentDatabaseId;
-document.getElementById("session-id").innerHTML = currentDatabaseId;
-const mContainer = document.getElementById("main-container");
-const budgetTableId = "budget-table";
-const btnSaveBudgetId = "btn-save-budget";
-const btnCancelBudgetId = "btn-cancel-budget";
-const budgetFormAlertId = "budget-form-alert";
-const totalBalanceId = "txt-total-balance";
-const totalCreditId = "txt-total-credit";
-const totalDebitId = "txt-total-debit";
+const app = document.getElementById("app");
 
-async function addBudget() {
-    const title = util.getVal("txt-title");
-    const notes = util.getVal("txt-notes");
-    const amount = util.getVal("txt-amount");
-    const rDebit = util.getVal("radio-debit");
-    const type = rDebit ? "d" : "c";
+const initApp = () => {
+    const navContainerId = util.uuid();
+    const btnAddEntryId = util.uuid();
+    const btnSettingsId = util.uuid();
+    const btnAboutId = util.uuid();
+    const totalBalanceId = util.uuid(); 
+    const totalCreditId = util.uuid(); 
+    const totalDebitId = util.uuid(); 
+    const budgetTableId = util.uuid();
+    
+    let showAddEditBudgetForm = ()=>{};
+    let budgetsList = [];
+    
+    util.setHtml(app.id, "");
 
-    if(amount === "" || title === ""){
-        util.setHtml(budgetFormAlertId, templates.alert("Please fill required fields", "warning"));
-        return;
-    } else { util.setHtml(budgetFormAlertId, ""); }
+    util.appendHtml(app.id, templates.navBar(navContainerId));
+    util.appendHtml(navContainerId, templates.button(btnAddEntryId, "Add Entry", "btn-primary m-1"));
+    util.appendHtml(navContainerId, templates.button(btnSettingsId, "Budget Settings", "btn-dark m-1"));
+    util.appendHtml(navContainerId, templates.button(btnAboutId, "About", "btn-light m-1"));
 
-    util.setButton(btnSaveBudgetId, templates.loadingButton(), true);
-    util.setButton(btnCancelBudgetId, "Cancel", true);
+    util.appendHtml(app.id, templates.mainPage(totalBalanceId, totalCreditId, totalDebitId, budgetTableId));
 
-    const response = await api.addBudget({ amount, title, notes, groupId: currentDatabaseId, type });
+    const setDbId = (newId)=>{
+        let id = newId || util.uuid();
+        document.cookie = "id="+id;
+        return id;
+    };
 
-    util.setButton(btnSaveBudgetId, "Save", false);
-    util.setButton(btnCancelBudgetId, "Cancel", false);
+    let currentDatabaseId = setDbId(document.cookie.split("=")[1]);
 
-    if(response){
-        budgetsList.push(response);
-        renderBudgetTable();
-        util.triggerClick(btnCancelBudgetId);
-    } else {
-        util.setHtml(budgetFormAlertId, templates.alert("Error Saving Data", "danger"));
-    }
-};
-
-const calculateBudget = () => {
-    let totalBalance = 0;
-    let totalCredit = 0;
-    let totalDebit = 0;
-
-    if(budgetsList.length !== 0){ 
-        budgetsList.forEach(bObject => {
-            if(bObject.type === "d"){
-                totalDebit += bObject.amount;
-            } else {
-                totalCredit += bObject.amount;
-            }
-        });
-    }
-
-    totalBalance = totalCredit - totalDebit;
-
-    util.setHtml(totalBalanceId, `${totalBalance} KWD`);
-    util.setHtml(totalCreditId, `${totalCredit} KWD`);
-    util.setHtml(totalDebitId, `${totalDebit} KWD`);
-};
-
-const renderBudgetTable = ()=>{
-    util.setHtml(budgetTableId, "");
-    if(budgetsList.length === 0){ util.setHtml(budgetTableId, templates.emptyTable()); }
-    budgetsList.forEach(bObject => {
-        bObject.btnEdit = util.uuid();
-        bObject.btnDelete = util.uuid();
-        bObject.btnInfo = util.uuid();
-        bObject.containerId = util.uuid();
-
-        util.appendHtml(budgetTableId, templates.budgetRecord(bObject));
-
-        util.onClick(bObject.btnDelete, ()=>{
-            if(confirm("Are you sure you want to delete this record?")){
-                if(api.deleteBudget(bObject._id)){
-                    for(let i = 0; i< budgetsList.length; i++){
-                        if(budgetsList[i]._id === bObject._id){
-                            budgetsList.splice(i, 1); break;
-                        }
-                    }
-                    renderBudgetTable();
+    const calculateBudget = () => {
+        let totalBalance = 0;
+        let totalCredit = 0;
+        let totalDebit = 0;
+    
+        if(budgetsList.length !== 0){ 
+            budgetsList.forEach(bObject => {
+                if(bObject.type === "d"){
+                    totalDebit += bObject.amount;
+                } else {
+                    totalCredit += bObject.amount;
                 }
+            });
+        }
+    
+        totalBalance = totalCredit - totalDebit;
+    
+        util.setHtml(totalBalanceId, `${totalBalance} KWD`);
+        util.setHtml(totalCreditId, `${totalCredit} KWD`);
+        util.setHtml(totalDebitId, `${totalDebit} KWD`);
+    };
+
+    const updateBudget = (updatedBudgetObject) => {
+        for(let i = 0; i < budgetsList.length; i++){
+            if(budgetsList[i]._id === updatedBudgetObject._id){
+                budgetsList[i] = updatedBudgetObject;
+                break;
+            }
+        }
+    };
+    
+    const renderBudgetTable = ()=>{
+        util.setHtml(budgetTableId, "");
+        if(budgetsList.length === 0){ util.setHtml(budgetTableId, templates.emptyTable()); }
+        const debitList = [];
+        const creditList = [];
+
+        budgetsList.forEach(bObj =>{
+            bObj.btnEdit = util.uuid();
+            bObj.btnDelete = util.uuid();
+            bObj.btnInfo = util.uuid();
+            bObj.containerId = util.uuid();
+
+            if(bObj.type === "d"){
+                debitList.push(bObj);
+            } else {
+                creditList.push(bObj);
             }
         });
+
+        const tempOrderedList = [...creditList, ...debitList];
+
+        tempOrderedList.forEach(bObject => {    
+            util.appendHtml(budgetTableId, templates.budgetRecord(bObject));
+    
+            util.onClick(bObject.btnDelete, ()=>{
+                if(confirm("Are you sure you want to delete this record?")){
+                    if(api.deleteBudget(bObject._id)){
+                        for(let i = 0; i< budgetsList.length; i++){
+                            if(budgetsList[i]._id === bObject._id){
+                                budgetsList.splice(i, 1); break;
+                            }
+                        }
+                        renderBudgetTable();
+                    }
+                }
+            });
+
+            util.onClick(bObject.btnInfo, ()=>{
+                const modalId = util.uuid();
+                const footerId = util.uuid();
+                const bodyId = util.uuid();
+                const btnCancelId = util.uuid();
+
+                util.initModal(modalId, 
+                    "Details", 
+                    bodyId,
+                    footerId);
+
+                util.setHtml(bodyId, templates.budgetInfo(bObject));
+
+                util.setHtml(footerId, templates.button(btnCancelId, "Close", "btn-secondary"));
+
+                util.onClick(btnCancelId, ()=>{
+                    util.hideModal(modalId);
+                });
+                
+                util.showModal(modalId);
+            });
+
+            util.onClick(bObject.btnEdit, ()=>{
+                showAddEditBudgetForm(bObject);
+            });
+        });
+        calculateBudget();
+    };
+    
+    const loadBudget = async () => {
+        util.setHtml(budgetTableId, templates.loadingTable());
+        budgetsList = await api.getBudgetsByGroup(currentDatabaseId);
+        renderBudgetTable();
+    };
+    
+    showAddEditBudgetForm = async (formObject) => {
+        const modalId = util.uuid();
+        const footerId = util.uuid();
+        const bodyId = util.uuid();
+        const radioCreditId = util.uuid();
+        const radioDebitId = util.uuid();
+        const titleId = util.uuid();
+        const notesId = util.uuid();
+        const amountId = util.uuid();
+        const alertId = util.uuid();
+        const btnCancelId = util.uuid();
+        const btnSaveId = util.uuid();
+    
+        const editMode = formObject ? true : false;
+    
+        util.initModal(modalId, 
+            editMode ? "Edit Budget" : "Add Budget", 
+            bodyId,
+            footerId);
+        
+        util.setHtml(bodyId, templates.formAddEditBudget(radioDebitId, radioCreditId, titleId, notesId, amountId, alertId));
+        util.setHtml(footerId, templates.button(btnCancelId, "Cancel", "btn-secondary") + templates.button(btnSaveId, editMode ? "Save" : "Add", "btn-primary"));
+        
+        util.onClick(btnCancelId, ()=>{
+            util.hideModal(modalId);
+        });
+    
+        if(editMode){
+            util.setVal(titleId, formObject.title);
+            util.setVal(notesId, formObject.notes);
+            util.setVal(amountId, formObject.amount);
+            util.setVal(radioDebitId, formObject.type === "d");
+            util.setVal(radioCreditId, formObject.type === "c");
+        }
+
+        util.onClick(btnSaveId, async () => {
+            const title = util.getVal(titleId);
+            const notes = util.getVal(notesId);
+            const amount = util.getVal(amountId);
+            const rDebit = util.getVal(radioDebitId);
+            const type = rDebit ? "d" : "c";
+        
+            if(amount === "" || title === ""){
+                util.setHtml(alertId, templates.alert("Please fill required fields", "warning"));
+                return;
+            } else { util.setHtml(alertId, ""); }
+        
+            util.setButton(btnSaveId, templates.loadingButton(), true);
+            util.setButton(btnCancelId, "Cancel", true);
+        
+            let serverBudgetObject = null;
+    
+            if(editMode) {
+                serverBudgetObject = await api.updateBudget({ id: formObject._id, amount, title, notes, type });
+            } else {
+                serverBudgetObject = await api.addBudget({ amount, title, notes, groupId: currentDatabaseId, type });
+            }
+        
+            util.setButton(btnSaveId, "Save", false);
+            util.setButton(btnCancelId, "Cancel", false);
+        
+            if(serverBudgetObject){
+                if(editMode){
+                    updateBudget(serverBudgetObject);
+                } else {
+                    budgetsList.push(serverBudgetObject);
+                }
+                
+                renderBudgetTable();
+                util.hideModal(modalId);
+            } else {
+                console.log(editMode);
+                util.setHtml(alertId, templates.alert("Error Saving Data", "danger"));
+            }
+        });
+    
+        util.showModal(modalId);
+    };
+    
+    const showBudgetSettingsForm = () => {
+        const modalId = util.uuid();
+        const footerId = util.uuid();
+        const bodyId = util.uuid();
+        const btnCancelId = util.uuid();
+        const btnSaveId = util.uuid();
+        const sessionId = util.uuid();
+        const sessionInputId = util.uuid();
+    
+        util.initModal(modalId, 
+            "Budget Settings", 
+            bodyId,
+            footerId);
+        
+        util.setHtml(bodyId, templates.formBudgetSettings(sessionId, sessionInputId));
+    
+        util.setHtml(footerId, templates.button(btnCancelId, "Cancel", "btn-secondary") + templates.button(btnSaveId, "Save", "btn-primary"));
+        
+        util.setHtml(sessionId, currentDatabaseId);
+    
+        util.onClick(btnCancelId, ()=>{
+            util.hideModal(modalId);
+        });
+
+        util.onClick(btnSaveId, ()=>{
+            currentDatabaseId = setDbId(util.getVal(sessionInputId));
+            util.hideModal(modalId);
+            loadBudget();
+        });
+        
+        util.showModal(modalId);
+    };
+    
+    const showAbout = () => {
+        const modalId = util.uuid();
+        const footerId = util.uuid();
+        const bodyId = util.uuid();
+        const btnCancelId = util.uuid();
+    
+        util.initModal(modalId, 
+            "About", 
+            bodyId,
+            footerId);
+    
+        util.setHtml(bodyId, templates.aboutBudget());
+    
+        util.setHtml(footerId, templates.button(btnCancelId, "Close", "btn-secondary"));
+              
+        util.onClick(btnCancelId, ()=>{
+            util.hideModal(modalId);
+        });
+        
+        util.showModal(modalId);
+    };
+    
+    util.onClick(btnAddEntryId, ()=>{
+        showAddEditBudgetForm(null);
     });
-    calculateBudget();
+    
+    util.onClick(btnSettingsId, ()=>{
+        showBudgetSettingsForm();
+    });
+    
+    util.onClick(btnAboutId, ()=>{
+        showAbout();
+    });
+    
+    loadBudget();
+    
+    util.initMobileMenu();
 };
 
-async function loadBudget() {
-    util.setHtml(budgetTableId, templates.loadingTable());
-    budgetsList = await api.getBudgetsByGroup(currentDatabaseId);
-    renderBudgetTable();
-};
-
-loadBudget();
-
-util.onClick(btnSaveBudgetId, ()=>{
-    addBudget();
-});
+initApp();
